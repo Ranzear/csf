85189a9fae2387feb140d475f21392f92c27bf49
Ranzear <Ranzear@gmail.com>
2013-10-25 16:16:05 -0700

    HW3 complete.
    Included original (slow) script with mild modification for performance
    comparison.

--- HW3/dna_analysis.py
+++ HW3/dna_analysis.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Royce Jensen
+# Evergreen Login: jenroy30
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 3: DNA analysis (Part 1)
@@ -16,6 +16,9 @@
 # The sys module supports reading files, command-line arguments, etc.
 import sys
 
+# Time module for performance interest, uncomment with lines ~37, ~94
+# from time import time
+
 
 ###########################################################################
 ### Read the nucleotides into a variable named seq
@@ -27,47 +30,73 @@ if len(sys.argv) < 2:
     sys.exit(2)
 # The file name specified on the command line, as a string.
 filename = sys.argv[1]
+
+# File extension check, because I got bored.
+if filename.split('.')[len(filename.split('.'))-1] != 'fastq':
+    print "Sorry, this script only handles .fastq format."
+    sys.exit(2)
+
 # A file object from which data can be read.
 inputfile = open(filename)
+# (I really wanted to add a try/except block on this, but open() throws its own)
 
-# All the nucleotides in the input file that have been read so far.
-seq = ""
-# The current line number (= the number of lines read so far).
-linenum = 0
+# For performance interest
+# start_time = time()
 
+# Count of lines read for validation against original. Uncomment lines ~64, ~93
+# linescounted = 0
 
-for line in inputfile:
-    linenum = linenum + 1
-    # if we are on the 2nd, 6th, 10th line...
-    if linenum % 4 == 2:
-        # Remove the newline characters from the end of the line
-        line = line.rstrip()
-        seq = seq + line
-
-
-###########################################################################
-### Compute statistics
-###
+# Count invalid pairs in total, or False to count only valid ATGC pairs
+count_invalid = True
 
 # Total nucleotides seen so far.
 total_count = 0
 # Number of G and C nucleotides seen so far.
 gc_count = 0
+# Number of A and T nucleotides seen so far.
+at_count = 0
 
+# Reads .fastq files by format specifics instead of guessing where the useful
+# data is. Added per-line counting instead of strip-and-load gymnastics.
 
-# for each base pair in the string,
-for bp in seq:
-    # increment the total number of bps we've seen
-    total_count = total_count + 1
+# http://en.wikipedia.org/wiki/FASTQ_format
 
-    # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
-        # increment the count of gc
-        gc_count = gc_count + 1
+# When parsing, this activates counting on the next line to be read.
+readnext = False
 
+# Enumerate is super handy, but seriously what the hell, python? I can't just 
+# have the file lines indexed nicely?
+# http://docs.python.org/2/library/functions.html#enumerate
+for i, line in enumerate(inputfile):
+    if readnext == True: # Previous line started with an '@'
+        # linescounted += 1 # For line count check.
+        for bp in line.rstrip():
+            if bp == 'C' or bp == 'G':
+                total_count += 1 # Increment total count only on valid counts
+                gc_count += 1
+            elif bp == 'A' or bp == 'T':
+                total_count += 1
+                at_count += 1
+            elif count_invalid == True:
+                total_count += 1
+        readnext = False
+    elif readnext == False and line[0] == '@': # Read first character of line.
+        readnext = True # Count pairs in next line
 
-# divide the gc_count by the total_count
+# This now takes about twelve seconds to count two types, versus almost a minute
+# to count just one.
+
+# divide the gc_count and ac_count by the total_count
 gc_content = float(gc_count) / total_count
+at_content = float(at_count) / total_count
 
 # Print the answer
 print 'GC-content:', gc_content
+print 'AT-content:', at_content
+
+# Extra count checking
+# print 'G/C pairs', gc_count
+# print 'A/T pairs', at_count
+
+# print 'lines counted', linescounted
+# print 'in ', time() - start_time, "seconds"
